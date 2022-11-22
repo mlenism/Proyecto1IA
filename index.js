1e2e3d4727ce1100abc63d01fb32f93c5afa4eda
mlenism <mlenism@unal.edu.co>
2022-11-22 07:47:03 -0500

    commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-//////////////////// ESCENARIO ////////////////////
+//////////////// MOSTRAR ESCENARIO ////////////////
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
 
 const canvasDimention = 800;
 const sizePercentage = 0.8;
+let = nodosExpandidos = 0;
+let = nodosTotales = 1;
+let profundidad = 0;
+let tiempo = 0;
 
-const matrix = []
 const sprites = new Map();
 async function loadImages() {
     sprites.set('2', await addImage('player.png'))
@@ -16,19 +19,21 @@ async function loadImages() {
     sprites.set('6', await addImage('goal.png'))
 }
 
-function drawSquares(width, height) {
-    const squareWidth = canvasDimention/width;
-    const squareHeight = canvasDimention/height;
-    for (let i=0; i<width; i++) {
-        for (let j=0; j<height; j++) {
-            squareColor(squareWidth, squareHeight, i, j)
+function drawSquares(matrix) {
+    const matrixWidth = matrix[0].length;
+    const matrixLength = matrix.length;
+    const squareWidth = canvasDimention/matrixWidth;
+    const squareHeight = canvasDimention/matrixLength;
+    for (let i=0; i<matrixWidth; i++) {
+        for (let j=0; j<matrixLength; j++) {
+            squareColor(matrix, squareWidth, squareHeight, i, j)
+            drawSprites(matrix, squareWidth,squareHeight, i, j)
             squareBorder(squareWidth, squareHeight, i, j)
-            drawSprites(squareWidth,squareHeight,i, j)
         }
     }
 }
 
-function squareColor(width, height, row, column) {
+function squareColor(matrix, width, height, row, column) {
     const color = matrix[row][column] == 1 ? 'saddlebrown' : 'white';
     context.fillStyle = color;
     context.fillRect(width*column, height*row, width, height);
@@ -46,7 +51,7 @@ function squareBorder(width, height, row, column) {
     context.stroke();
 }
 
-function drawSprites(cellWidth, cellHeight, row, column) {
+function drawSprites(matrix, cellWidth, cellHeight, row, column) {
     const img = sprites.get(matrix[row][column]) || null;
     if (img) {
         drawSprite(img,cellWidth, cellHeight, row, column)
@@ -70,27 +75,6 @@ function addImage(src) {
     })
 }
 
-function fillMatrix(txt) {
-    const text = txt.trim();
-    const rows = text.split('\r\n');
-    for (let i = 0; i<rows.length; i++) {
-        const columns = rows[i].split(' ');
-        matrix[i] = new Array(columns.length)
-        for (let j = 0; j<columns.length; j++) {
-            matrix[i][j] = columns[j];
-        }
-    }
-}
-
-fetch('escenario.txt').then(res => {
-    res.text().then(txt => {
-        fillMatrix(txt);
-        loadImages().then(()=>{
-            drawSquares(matrix[0].length, matrix.length);
-        });
-    })
-})
-
 //////////////////// FORMULARIO ////////////////////
 
 function radioButtonHandler(bool) {
@@ -110,89 +94,326 @@ function radioButtonHandler(bool) {
 }
 
 function runScene() {
+    nodosExpandidos = 0;
+    nodosTotales = 1;
+    profundidad = 0;
+    const node = new Node(matrix, null, '', 0, 0, 0, 0)
     const opciones1 = document.getElementById("opciones1");
     const opciones2 = document.getElementById("opciones2");
+    let moves = [];
     if (opciones1.style.display != 'none') {
         switch (opciones1.value) {
-            case 'amplitud': amplitud(); break;
-            case 'uniforme': costoUniforme(); break;
-            default: noCiclos(); break;
+            case 'amplitud': moves = aiAlgorithm(node, amplitud); break;
+            case 'uniforme': moves = aiAlgorithm(node, costoUniforme); break;
+            default: moves = aiAlgorithm(node, profundidadNoCiclos); break;
         }
     } else {
         switch (opciones2.value) {
-            case 'avara': avara(); break;
-            default: a(); break;
+            case 'avara': moves = aiAlgorithm(node, avara); break;
+            default: moves = aiAlgorithm(node, a); break;
         }
     }
+    animate(moves)
 }
 
 ////////////////// ALGORITMOS IA //////////////////
 
-function amplitud() {
-    animate(moves);
+function aiAlgorithm(node, updateQueue) {
+    const startTime = performance.now();
+    const queue = []
+    queue.push(node)
+    while (queue.length) {
+        let isExpanded = false;
+        const nd = queue.shift()
+        if (nd.isGoal()) {
+            tiempo = performance.now() - startTime;
+            return nd;
+        }
+        const move = movesChar(nd.stage, '2')
+        if (move.up && (nd.operator != 'D')) {
+            isExpanded = true;
+            updateQueue(queue, nd, move.up);
+        }
+        if (move.right && (nd.operator != 'L')) {
+            isExpanded = true;
+            updateQueue(queue, nd, move.right);
+        }
+        if (move.down && (nd.operator != 'U')) {
+            isExpanded = true;
+            updateQueue(queue, nd, move.down);
+        }
+        if (move.left && (nd.operator != 'R')) {
+            isExpanded = true;
+            updateQueue(queue, nd, move.left);
+        }
+        if (isExpanded) {
+            nodosExpandidos++;
+        }
+    }
 }
 
-function costoUniforme() {
-    console.log("costo uniforme");
+function amplitud(queue, nd, operator) {
+    const leaf = makeLeaf(nd, operator)
+    queue.push(leaf);
 }
 
-function noCiclos() {
-    console.log("profundidad evitando ciclos");
+
+function profundidadNoCiclos(queue, nd, operator) {
+    const leaf = makeLeaf(nd, operator)
+    if (!leaf.isVIsited(nd)) {
+        leaf.addVIsited(nd)
+        queue.unshift(leaf)
+    }
 }
 
-function avara() {
-    console.log("Avara");
+function costoUniforme(queue, nd, operator) {
+    const leaf = makeLeaf(nd, operator)
+    for (let i=0; i<=queue.length; i++) {
+        if (i == queue.length) {
+            queue.push(leaf);
+            break;
+        } else if (leaf.weight <= queue[i].weight) {
+            queue.splice(i,0,leaf);
+            break;
+        }
+    }
 }
 
-function a() {
-    console.log("a*");
+function avara(queue, nd, operator) {
+    const leaf = makeLeaf(nd, operator)
+    const rule = (j) => leaf.getHeuristic() <= queue[j].getHeuristic();
+    addToQueue(leaf, queue, nd, rule) 
 }
 
-function playerPos() {
+function a(queue, nd, operator) {
+    const leaf = makeLeaf(nd, operator)
+    const rule = (j) => leaf.getHeuristic()+leaf.weight <= queue[j].getHeuristic()+queue[j].weight;
+    addToQueue(leaf, queue, nd, rule) 
+}
+
+function addToQueue(leaf, queue, nd, rule) {
+    for (let i=0; i<=queue.length; i++) {
+        if (!leaf.isVIsited(nd)) {
+            leaf.addVIsited(nd);
+            if (i == queue.length) {
+                queue.push(leaf);
+                break
+            } else if (rule(i)) {
+                queue.splice(i, 0, leaf);
+                break
+            } 
+        }
+    }
+}
+
+function makeLeaf(nd, operator) {
+    const move = nd.applyOperator(operator);
+    nodosTotales++;
+    if (nd.depth > profundidad) {
+        profundidad = nd.depth
+    }
+    return new Node(move.child, nd, operator, nd.depth+1, move.weight, move.flames, move.stars);
+}
+
+class Node {
+    constructor(stage, parent, operator, depth, weight, flames, stars){
+        this.stage = stage.map((arr) => arr.slice());
+        this.parent = parent;
+        this.operator = operator;
+        this.depth = depth;
+        this.weight = weight;
+        this.flames = flames;
+        this.stars = stars;
+        this.visited = parent ? parent.visited.slice() : [];
+    }
+    applyOperator(operator) {
+        const child = this.stage.map((arr) => arr.slice());
+        const properties = moveOne(operator, child, '2', this)
+        return {
+            child: child,
+            weight: properties.weight,
+            flames: properties.flames,
+            stars: properties.stars
+        }
+    }
+    isGoal() {
+        const playerPosStr = JSON.stringify(playerPos(this.stage,'2'))
+        const goalPosStr = JSON.stringify(playerPos(matrix,'6'))
+        return playerPosStr == goalPosStr
+    }
+    hasFlames() {
+        return this.flames > 0;
+    }
+    hasStars() {
+        return this.stars > 0;
+    }
+    isVIsited(node) {
+        const pos = playerPos(node.stage, '2')
+        const v = this.visited.some(p=>JSON.stringify(p)==JSON.stringify(pos))
+        return v
+    }
+    addVIsited(node) {
+        const pos = playerPos(node.stage, '2')
+        this.visited.push(pos)
+    }
+    getHeuristic() {
+        const p = playerPos(this.stage, '2');
+        const g = playerPos(this.stage, '6');
+        if (g) {
+            return Math.sqrt(Math.pow(p[0]-g[0],2)+Math.pow(p[1]-g[1],2))
+        }
+        return 0
+    }
+    getParent(node) {
+        let relative = this;
+        for(let i = 0; i<node-1; i++) {
+            relative = relative.parent
+        }
+        return relative;
+    }
+}
+
+//////////////////// MOVEMENT ////////////////////
+
+function playerPos(matrix, player) {
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            if(matrix[i][j] == '2') {
+            if(matrix[i][j] == player) {
                 return [i,j]
             }
         }
     }
 }
 
-//////////////////// ANIMACIÓN ////////////////////
+function moveOne(move, mtrx, player, node) {
+    const pos = playerPos(mtrx, player)
+    let playerPlace = node.parent ? node.parent.stage[pos[0]][pos[1]] : '0';
+    const nodeProperties = {
+        weight: node.weight,
+        stars: node.stars,
+        flames: node.flames
+    }
+    if (move == 'U') {
+        mtrx[pos[0] - 1][pos[1]] = player;
+    } else if (move == 'D') {
+        mtrx[pos[0] + 1][pos[1]] = player;
+    } else if (move == 'L') {
+        mtrx[pos[0]][pos[1] - 1] = player;
+    } else if (move == 'R') {
+        mtrx[pos[0]][pos[1] + 1] = player;
+    }
+    if (playerPlace == '0') {
+        mtrx[pos[0]][pos[1]] = '0';
+        if (node.hasStars()) {
+            nodeProperties.stars--;
+            nodeProperties.weight+=0.5;
+        } else {
+            nodeProperties.weight++;
+        }
+    } else if (playerPlace == '3') {
+        if (node.hasFlames()) {
+            mtrx[pos[0]][pos[1]] = '3';
+            nodeProperties.weight++;
+        } else if (node.hasStars()) {
+            mtrx[pos[0]][pos[1]] = '0';
+            nodeProperties.stars--;
+            nodeProperties.stars+=6;
+            nodeProperties.weight+=0.5;
+        } else {
+            mtrx[pos[0]][pos[1]] = '0';
+            nodeProperties.weight++;
+            nodeProperties.stars+=6;
+        }
+    } else if (playerPlace == '4') {
+        if (node.hasStars()) {
+            mtrx[pos[0]][pos[1]] = '4';
+            nodeProperties.stars--;
+            nodeProperties.weight+=0.5;
+        } else {
+            mtrx[pos[0]][pos[1]] = '0';
+            nodeProperties.weight++;
+            nodeProperties.flames++;
+        }
+    } else if (playerPlace == '5') {
+        if (node.hasFlames()) {
+            mtrx[pos[0]][pos[1]] = '0';
+            nodeProperties.weight++;
+            nodeProperties.flames--;
+        } else if (node.hasStars()) {
+            mtrx[pos[0]][pos[1]] = '5';
+            nodeProperties.stars--;
+            nodeProperties.weight+=0.5;
+        } else {
+            mtrx[pos[0]][pos[1]] = '5'
+            nodeProperties.weight+=6;
+        }
+    }
+    return nodeProperties;
+}
 
-const velocity = 250;
-const moves = ['D','R','R','D','R','R','R','R','R','R','R','D','D','L','L']
-
-async function animate(moves) {
-    for (const e of moves) {
-        moveOne(e);
-        await sleep(velocity)
+function movesChar(matrix, player) {
+    const pos = playerPos(matrix, player)
+    const U = matrix[pos[0] - 1] ? matrix[pos[0] - 1][pos[1]] : false;
+    const D = matrix[pos[0] + 1] ? matrix[pos[0] + 1][pos[1]] : false;
+    const L = matrix[pos[0]][pos[1] - 1] || false;
+    const R = matrix[pos[0]][pos[1] + 1] || false;
+    return {
+        up:  U && (U != '1') ? 'U': false,
+        down: D && (D != '1') ? 'D': false,
+        left: L && (L != '1') ? 'L': false,
+        right: R && (R != '1') ? 'R': false,
     }
 }
 
-function moveOne(move) {
-    const pos = playerPos();
-    if (move.length == 1) {
-        matrix[pos[0]][pos[1]] = '0';
-    } else if (move.substring(1,2) == 'S') {
-        matrix[pos[0]][pos[1]] = '3';
-    } else if (move.substring(1,2) == 'F') {
-        matrix[pos[0]][pos[1]] = '4';
-    } else if (move.substring(1,2) == 'K') {
-        matrix[pos[0]][pos[1]] = '5';
+//////////////////// ANIMACIÓN ////////////////////
+
+const velocity = 250;
+
+async function animate(moves) {
+    for (let i=moves.depth; i>0; i--) {
+        const parent = moves.getParent(i);
+        drawSquares(parent.stage);
+        await sleep(velocity)
     }
-    if (move.substring(0,1) == 'T') {
-        matrix[pos[0] - 1][pos[1]] = '2';
-    } else if (move.substring(0,1) == 'D') {
-        matrix[pos[0] + 1][pos[1]] = '2';
-    } else if (move.substring(0,1) == 'L') {
-        matrix[pos[0]][pos[1] - 1] = '2';
-    } else if (move.substring(0,1) == 'R') {
-        matrix[pos[0]][pos[1] + 1] = '2';
-    }
-    drawSquares(matrix[0].length, matrix.length);
+    showResults()
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+/////////////// MOSTRAR RESULTADOS ///////////////
+
+function showResults() {
+    document.getElementById("resultados").style.display = "block";
+    const parrafo = document.getElementById("parrafo");
+    parrafo.innerHTML =  `Nodos expandidos: ${nodosExpandidos}\n`;
+    parrafo.innerHTML +=  `Nodos Totales: ${nodosTotales}\n`;
+    parrafo.innerHTML += `Profundidad del arbol: ${profundidad}\n`;
+    parrafo.innerHTML += `Tiempo de computo: ${tiempo} ms`;
+}
+
+//////////////// CARGAR ESCENARIO ////////////////
+
+const matrix = []
+
+function fillMatrix(txt, matrix) {
+    const text = txt.trim();
+    const rows = text.split('\r\n');
+    for (let i = 0; i<rows.length; i++) {
+        const columns = rows[i].split(' ');
+        matrix[i] = new Array(columns.length)
+        for (let j = 0; j<columns.length; j++) {
+            matrix[i][j] = columns[j];
+        }
+    }
+}
+
+fetch('escenario.txt').then(res => {
+    res.text().then(txt => {
+        fillMatrix(txt, matrix);
+        loadImages().then(()=>{
+            drawSquares(matrix);
+        });
+    })
+})
